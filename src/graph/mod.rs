aff960c50bedaf714e03b6bbb1ae5d92881d58e1
Syukron Rifail M <syukronrifai@gmail.com>
2020-02-18 21:42:05 +0700

    enhance data structure

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,9 +1,16 @@
+use petgraph::{Graph as PetGraph, Undirected};
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
 
+pub type DimensionIndex = i8;
+pub type ObjectId = i32;
+pub type Scope = HashMap<Vec<DimensionIndex>, Pair>;
+pub type Pair = HashMap<ObjectId, Vec<Range>>;
+
 #[allow(dead_code)]
 pub struct Structure {
-
+    graph: RefCell<PetGraph<Node, Edge, Undirected>>,
 }
 
 #[allow(dead_code)]
@@ -19,24 +26,19 @@ pub struct Edge {
     len: f32,
     ni: Rc<Node>,
     nj: Rc<Node>,
-    scope: Scope,
-    sky_scope: Scope,
-    two_scope: Scope,
-    d_scope: Scope,
-    k_skyline_scope: Scope,
+    objects: RefCell<Vec<Object>>,
+    scope: RefCell<Scope>,
+    sky_scope: RefCell<Scope>,
+    two_sky_scope: RefCell<Scope>,
+    d_sky_scope: RefCell<Scope>,
+    k_sky_scope: RefCell<Scope>,
 }
-
-#[allow(dead_code)]
-pub type Scope = HashMap<Vec<i8>, Pair>;
 
 #[allow(dead_code)]
 pub struct Object {
-    id: i32,
+    id: ObjectId,
     attr: Vec<f32>,
 }
-
-#[allow(dead_code)]
-pub type Pair = HashMap<i32, Vec<Range>>;       // HashMap<ObjectId,....>
 
 #[allow(dead_code)]
 pub struct Range {

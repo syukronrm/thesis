864a5ec2eb64f36892c13b22cff8e3632baafc48
Syukron Rifail M <syukronrifai@gmail.com>
2020-03-08 13:01:47 +0700

    remove pseudo

--- src/structure/voronoi/mod.rs
+++ src/structure/voronoi/mod.rs
@@ -10,31 +10,6 @@ pub mod state;
 #[derive(Debug)]
 pub struct Voronoi(HashMap<EdgeIndex, Vec<Range>>);
 
-/// find voronoi:
-///   // input: &graph, source_centroid, centroid_ids, max_distance
-///   // outout: voronoi
-///   mut voronoi = hashmap<_>
-///   mut visited = hashmap<nodeid, (dist, centroid_id, edge_pred)>
-///   let q = binaryheap<state>
-///   initial enqueue 2 neigbor of centroids and its distance
-///   while q not empty do:
-///     n = q.dequeue
-///     for each edge e from n to m in n.neighbors() and m not visited by n.centroid_id do
-///       dist_m = n.distance + e.len
-///       if m is not visited by any centroid:
-///         if dist_m < max_distance:
-///           q.enqueue(state(m, q.centroid_id, dist_m))
-///           visited.insert(m.id, (dist_m, q.centroid_id))
-///           if q.centroid_id == source_centroid:
-///             insert e to voronoi till max_distance
-///       else if m is visited by another centroid:
-///         if dist_m < existing distance:
-///           replace visited with a new arrived q
-///           replace queued node with a new arrived q
-///         else
-///           if q.centroid_id == source_centroid:
-///             compute voronoi in e with node visited.node and q.node
-
 impl Voronoi {
     #[allow(dead_code)]
     pub fn new() -> Voronoi {

feed58fd4e3d70f1eabbceb2497e0dfcfa5fb98a
Syukron Rifail M <syukronrifai@gmail.com>
2020-03-08 17:44:17 +0700

    - refactor preparing graph - structure for query and pair

--- src/algo/mod.rs
+++ src/algo/mod.rs
@@ -1,50 +1,5 @@
-use petgraph::stable_graph::StableGraph;
-use std::collections::HashMap;
-use std::path::PathBuf;
-use std::rc::Rc;
-
-use crate::dataset::{Edge as DataEdge, Node as DataNode};
-use crate::structure::{Edge, Graph, Node, PetgraphNodeEdge};
-
-use crate::dataset::load_edges;
-
 pub mod voronoi;
+pub mod query;
+pub mod prepare_graph;
 
-#[allow(dead_code)]
-pub fn create_initial_graph(dataset_dir: PathBuf, node_csv: &str, edge_csv: &str) -> Graph {
-    let edges = load_edges(dataset_dir, node_csv, edge_csv);
-    prepare_graph(edges)
-}
-
-#[allow(dead_code)]
-fn prepare_graph(edges: Vec<DataEdge>) -> Graph {
-    let mut graph: PetgraphNodeEdge = StableGraph::with_capacity(0, 0);
-    let mut added_node_ids = HashMap::new();
-
-    let mut get_node_index =
-        move |node: &Rc<DataNode>, graph: &mut PetgraphNodeEdge| match added_node_ids.get(&node.id)
-        {
-            Some(node_index) => *node_index,
-            None => {
-                let node_index = graph.add_node(Node {
-                    id: node.id,
-                    lng: node.lng,
-                    lat: node.lat,
-                });
-                added_node_ids.insert(node.id, node_index);
-                node_index
-            }
-        };
-
-    for edge in edges {
-        let graph_ni = get_node_index(&edge.ni, &mut graph);
-        let graph_nj = get_node_index(&edge.nj, &mut graph);
-        graph.add_edge(
-            graph_ni,
-            graph_nj,
-            Edge::new(edge.id, edge.len, edge.ni.id, edge.nj.id),
-        );
-    }
-
-    Graph::new(graph)
-}
+pub use prepare_graph::create_initial_graph;
---
+++ src/algo/query.rs
@@ -0,0 +1,55 @@
+pub struct Pair(pub usize, pub usize);
+
+pub struct Multiqueries(Vec<Vec<usize>>);
+
+impl Multiqueries {
+  pub fn new() -> Multiqueries {
+    Multiqueries(Vec::new())
+  }
+
+  pub fn insert(&mut self, query: Vec<usize>) {
+    self.0.push(query);
+  }
+
+  pub fn create_pairs(&self) -> Vec<Pair> {
+    let mut dimensions = Vec::new();
+    for query in &self.0 {
+      for val in query {
+        dimensions.push(*val);
+      }
+    }
+    dimensions.sort();
+    dimensions.dedup();
+
+    let mut pairs = Vec::new();
+
+    for di in &dimensions {
+      for dj in &dimensions {
+        if dj <= di { continue; }
+        pairs.push(Pair(*di, *dj));
+      }
+    }
+
+    pairs
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn multiqueries() {
+    let mut m = Multiqueries::new();
+    m.insert(vec![1, 2, 3]);
+    m.insert(vec![2, 3]);
+    m.insert(vec![2, 3, 6]);
+
+    let pairs = m.create_pairs();
+    assert_eq!(pairs.len(), 6);
+
+    let p1 = pairs.get(0).unwrap();
+    assert_eq!(p1.0, 1);
+    assert_eq!(p1.1, 2);
+  }
+}

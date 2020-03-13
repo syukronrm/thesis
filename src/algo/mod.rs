9d30a6f8951ed939fec5196877c0f786348da1d5
Syukron Rifail M <syukronrifai@gmail.com>
2020-03-13 10:23:09 +0700

    move query

--- src/algo/mod.rs
+++ src/algo/mod.rs
@@ -1,5 +1,4 @@
 pub mod voronoi;
-pub mod query;
 pub mod prepare_graph;
 
 pub use prepare_graph::create_initial_graph;
--- src/structure/mod.rs
+++ src/structure/mod.rs
@@ -5,10 +5,12 @@ pub mod edge;
 pub mod graph;
 pub mod node;
 pub mod voronoi;
+pub mod query;
 
 pub use edge::*;
 pub use graph::Graph;
 pub use node::Node;
 pub use voronoi::state::State;
+pub use query::{Multiqueries, Pair};
 
 pub type PetgraphNodeEdge = PetGraph<Node, Edge, Undirected>;
